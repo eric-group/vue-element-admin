6bd3b75cc2a39b3e653b6dad2ff3cb2718474a75
Eric <330886959@qq.com>
2019-05-15 17:30:35 +0800

    demo 菜单配置,基本跟老版本ccs系统的菜单一致.后续如果需要在根据具体情况改动. 这次只做了前端的配置,后期再跟后台匹配

---
+++ src/views/demo/system/permission/user/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <el-container>
+    <el-row>
+      <el-col :span="24">用户管理</el-col>
+    </el-row>
+  </el-container>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
